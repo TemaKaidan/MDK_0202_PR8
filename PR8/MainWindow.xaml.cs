85a5b941fc26f73348cd5ff2f5311a7e846e6c56
Artem <artemmohov05@mail.ru>
2024-12-11 02:19:45 +0500

    Написан метод получения данных о погоде. Мохов

--- PR8/MainWindow.xaml.cs
+++ PR8/MainWindow.xaml.cs
@@ -26,7 +26,6 @@ namespace PR8
         private const string ApiKey = "992a69bc60315f46a2587400486e1f4f";
         private const string ApiUrl = "https://api.openweathermap.org/data/2.5/forecast?q={0}&appid={1}&units=metric&lang=ru";
 
-
         public MainWindow()
         {
             InitializeComponent();
@@ -50,6 +49,39 @@ namespace PR8
         {
             string defaultCity = "Пермь";
 
+        }
+
+        private async Task<List<WeatherData>> FetchWeatherData(string city)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string url = string.Format(ApiUrl, city, ApiKey);
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception($"Ошибка {response.StatusCode}: {response.ReasonPhrase}");
+                }
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var json = JsonConvert.DeserializeObject<dynamic>(responseBody);
+
+                var weatherList = new List<WeatherData>();
+
+                foreach (var item in json.list)
+                {
+                    weatherList.Add(new WeatherData
+                    {
+                        DateTime = Convert.ToDateTime(item.dt_txt).ToString("dd.MM.yyyy HH:mm"),
+                        Temperature = $"{item.main.temp} °C",
+                        Pressure = $"{item.main.pressure} мм рт. ст.",
+                        Humidity = $"{item.main.humidity}%",
+                        WindSpeed = $"{item.wind.speed} м/с",
+                        FeelsLike = $"{item.main.feels_like} °C",
+                        WeatherDescription = item.weather[0].description.ToString(),
+                    });
+                }
+                return weatherList;
+            }
         }
     }
 }
